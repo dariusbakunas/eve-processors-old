77b0c74e46a4c7d2af2df03c3032b6a12ad1860a
Darius Bakunas-Milanowski <bakunas@gmail.com>
2020-01-01 15:49:05 -0500

    start breaking things up

--- esi.go
+++ esi.go
@@ -15,55 +15,96 @@ type PubSubMessage struct {
 	Data []byte `json:"data"`
 }
 
-func Esi(ctx context.Context, m PubSubMessage) error {
-	connectionName := os.Getenv("CLOUD_SQL_CONNECTION_NAME")
+func initializeDb() (*sql.DB, error) {
+	dbConnection := os.Getenv("DB_CONNECTION")
 
-	if connectionName == "" {
-		return fmt.Errorf("CLOUD_SQL_CONNECTION_NAME must be set")
+	if dbConnection == "" {
+		return nil, fmt.Errorf("DB_CONNECTION must be set")
+	}
+
+	dbDatabase := os.Getenv("DB_DATABASE")
+
+	if dbDatabase == "" {
+		return nil, fmt.Errorf("DB_DATABASE must be set")
 	}
 
 	dbUsername := os.Getenv("DB_USERNAME")
 
 	if dbUsername == "" {
-		return fmt.Errorf("DB_USERNAME must be set")
+		return nil, fmt.Errorf("DB_USERNAME must be set")
 	}
 
 	dbPassword := os.Getenv("DB_PASSWORD")
 
 	if dbPassword == "" {
-		return fmt.Errorf("DB_PASSWORD must be set")
+		return nil, fmt.Errorf("DB_PASSWORD must be set")
 	}
 
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@unix(/cloudsql/%s)/eve_gql_pd", dbUsername, dbPassword, connectionName))
+	var err error
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s/%s", dbUsername, dbPassword, dbConnection, dbDatabase))
 
 	if err != nil {
-		return fmt.Errorf("mysql: %v", err)
+		return nil, fmt.Errorf("mysql: %v", err)
 	}
 
-	rows, err := sq.Select("id, name").From("characters").RunWith(db).Query()
+	return db, nil
+}
+
+type Character struct {
+	id  int
+	accessToken string
+	refreshToken string
+	expires int
+}
+
+func getCharacters(db *sql.DB) ([]Character, error) {
+	rows, err := sq.Select("id, accessToken, refreshToken, expiresAt").From("characters").RunWith(db).Query()
 
 	if err != nil {
-		return fmt.Errorf("mysql: %v", err)
+		return nil, fmt.Errorf("mysql: %v", err)
 	}
 
 	defer rows.Close()
 
-	for rows.Next() {
-		var (
-			id int
-			name string
-		)
+	var characters []Character
 
-		err := rows.Scan(&id, &name)
+	for rows.Next() {
+		var character Character
+
+		err := rows.Scan(&character.id, &character.accessToken, &character.refreshToken, &character.expires)
 
 		if err != nil {
-			return fmt.Errorf("mysql: %v", err)
+			return nil, fmt.Errorf("mysql: %v", err)
 		}
 
-		log.Printf("Name: %s", name);
+		characters = append(characters, character)
+	}
+
+	return characters, nil
+}
+
+func Process() {
+	db, err := initializeDb()
+
+	if err != nil {
+		log.Fatalf("initializeDb: %v", err)
+	}
+
+	characters, err := getCharacters(db)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, character := range characters {
+		log.Printf("ID: %d", character.id)
 	}
 
 	defer db.Close()
+}
+
+func Esi(ctx context.Context, m PubSubMessage) error {
+	Process()
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.13
 require (
 	cloud.google.com/go v0.50.0
 	cloud.google.com/go/pubsub v1.0.1
-	github.com/Masterminds/squirrel v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/Masterminds/squirrel v1.1.0
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/joho/godotenv v1.3.0
 	google.golang.org/appengine v1.6.5 // indirect
 )
