d6f9e83e0a0b7f953e35b77d98ba66145f70e897
Darius Bakunas-Milanowski <bakunas@gmail.com>
2020-01-12 09:43:57 -0500

    do not execute insert within the loop, only do it once

--- db/skills.go
+++ db/skills.go
@@ -8,13 +8,13 @@ import (
 
 import sq "github.com/Masterminds/squirrel"
 
-func (d *DB) InsertSkills(characterID int64, skills []models.CharacterSkill) (int, int, error) {
+func (d *DB) InsertSkills(characterID int64, skills []models.CharacterSkill) (int64, int64, error) {
 	if len(skills) == 0 {
 		return 0, 0, nil
 	}
 
-	updated := 0
-	inserted := 0
+	updated := int64(0)
+	inserted := int64(0)
 
 	rows, err := squirrel.Select("skillId").
 		From("characterSkills").
@@ -41,6 +41,9 @@ func (d *DB) InsertSkills(characterID int64, skills []models.CharacterSkill) (in
 		idSet[id] = true
 	}
 
+	builder := squirrel.Insert("characterSkills").
+		Columns("skillId", "activeSkillLevel", "skillPointsInSkill", "trainedSkillLevel", "characterId")
+
 	for _, skill := range skills {
 		if idSet[skill.SkillID] {
 			_, err := squirrel.Update("characterSkills").
@@ -58,17 +61,22 @@ func (d *DB) InsertSkills(characterID int64, skills []models.CharacterSkill) (in
 
 			updated++
 		} else {
-			_, err := squirrel.Insert("characterSkills").
-				Columns("skillId", "activeSkillLevel", "skillPointsInSkill", "trainedSkillLevel", "characterId").
-				Values(skill.SkillID, skill.ActiveSkillLevel, skill.SP, skill.TrainedLevel, characterID).
-				RunWith(d.db).
-				Exec()
-
-			if err != nil {
-				return 0, 0, fmt.Errorf("squirrel.Insert: %v", err)
-			}
-
+			builder = builder.Values(skill.SkillID, skill.ActiveSkillLevel, skill.SP, skill.TrainedLevel, characterID)
 			inserted ++
+		}
+	}
+
+	if inserted > 0 {
+		result, err := builder.RunWith(d.db).Exec()
+
+		if err != nil {
+			return 0, 0, fmt.Errorf("builder.Exec: %v", err)
+		}
+
+		inserted, err = result.RowsAffected()
+
+		if err != nil {
+			return 0, 0, fmt.Errorf("result.RowsAffected: %v", err)
 		}
 	}
 
